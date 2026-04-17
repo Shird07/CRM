432cce65e146089098358cb52698a3492253e32f
Shird <shihabardi@gmail.com>
2026-04-17 18:01:57 +0700

    Initial commit: Uploading full Laravel project

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CRM Gadget</title>
+
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('css/landing.css') }}">
+</head>
+
+<body>
+
+<header class="navbar">
+  <div class="nav">
+    <div class="logo">Prototype</div>
+    <div>
+      <a href="#">Sign in</a>
+      <a href="#" class="btn">Sign up free</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a>
+    </div>
+  </div>
+</header>
+
+<section class="hero">
+
+  <div class="circle big"></div>
+  <div class="circle mid"></div>
+  <div class="circle small"></div>
+
+  <div class="left">
+    <h1>500+</h1>
+    <p>Brands</p>
+  </div>
+
+  <div class="right">
+    <h1>1000+</h1>
+    <p>Buyers</p>
+    <span>Affordable prices</span>
+  </div>
+
+  <div class="phone">
+    <img src="{{ asset('img/tanganHP.png') }}">
+  </div>
+
+</section>
+
+</body>
+</html>
